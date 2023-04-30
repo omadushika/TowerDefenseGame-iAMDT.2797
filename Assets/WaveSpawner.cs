27914094d4b684598abbe6f545c3275f9f8aba65
omadushika <madushikaoshan2018@gmail.com>
2023-04-30 16:19:47 +0530

    Wave Spawner

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveNumber = 1;
+
+    void Update ()
+    {
+        if (countdown <= 0f)
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    void SpawnWave ()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+
+       waveNumber++;
+       Debug.Log("Wave Incoming");
+    }
+
+    void SpawnEnemy ()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
